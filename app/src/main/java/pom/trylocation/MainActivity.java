5007a40d3ffbfc6d72ba1c854d667dbafc5adace
roy.leung <Roy.Leung@QBSs-MacBook-Air.local>
2016-11-14 12:26:57 +0800

    get xy from address

--- app/src/main/java/pom/trylocation/MainActivity.java
+++ app/src/main/java/pom/trylocation/MainActivity.java
@@ -1,22 +1,32 @@
 package pom.trylocation;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
 
@@ -25,6 +35,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @BindView(R.id.tvLocation)
     TextView tvLocation;
+    @BindView(R.id.ed_address)
+    EditText edAddress;
+    @BindView(R.id.bt_calculate)
+    Button btCalculate;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.activity_main)
+    LinearLayout activityMain;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
 
@@ -51,9 +69,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
 
     }
-//    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION})
-@NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION})
-     void getLocation(){
+
+    //    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION})
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void getLocation() {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLastLocation != null) {
@@ -62,6 +81,37 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             tvLocation.setText(latitude + " " + longitude);
 
         }
+    }
+
+
+    private LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void getLocationFromAddress() {
+        new getXYFromAddressAsyntask().execute(edAddress.getText().toString());
     }
 
     @Override
@@ -85,4 +135,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onStop();
     }
 
+
+    @OnClick(R.id.bt_calculate)
+    public void onClick() {
+        if (!edAddress.getText().toString().isEmpty()) {
+            getLocationFromAddress();
+        }
+    }
+
+    private class getXYFromAddressAsyntask extends AsyncTask<String, Void, LatLng> {
+        @Override
+        protected LatLng doInBackground(String... strings) {
+            return getLocationFromAddress(getApplicationContext(), strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(LatLng latLng) {
+            super.onPostExecute(latLng);
+            Toast.makeText(getApplicationContext(), latLng.latitude + "  " + latLng.longitude, Toast.LENGTH_SHORT).show();
+            Log.d("onPostExecute", latLng.latitude + "  " + latLng.longitude);
+        }
+    }
 }
